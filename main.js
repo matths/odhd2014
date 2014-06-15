b221d893d8424ac390855e80a20f9e2bf6e5fb96
Matthias Dittgen <matthias.dittgen@valiton.com>
2014-06-15 15:45:12 +0200

    fort und zuzüge

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ var path = d3.geo.path().projection(projection);
 
 var vis = d3.select("#stage").append("svg").attr("width", 960).attr("height", 600);
 
+var mode, age;
 var csv;
-
-colorMap = d3.scale.linear()
-	.domain([0,10])
-	.range(["#cccccc","#ff0000"]);
+var json;
+var age = "18-25";
 
 function loadDaten (callback) {
 	var dataset = [];
@@ -23,8 +22,8 @@ function loadDaten (callback) {
 	});	
 }
 
-function showMap(json) {
-	var kreise;
+var kreise;
+function showMap() {
 
     kreise = vis.selectAll("path")
 		.data(json.features)
@@ -37,36 +36,26 @@ function showMap(json) {
 				.attr("d", path)
 				.attr("stroke", "white")
 				.attr("stroke-width", 1)
-				.on('mouseover', function () {
-				});
-
-	kreise
-		.attr("fill", function (d, i) {
-			var bev = d.properties.daten["Arbeitslosenzahlen 2005"];
-			bev = bev.replace( /,/,"." );
-			bev = parseFloat(bev);
-//			console.log(i, d.properties.GEN, bev, colorMap(bev));
-			return colorMap(bev);
-		});
-
+	update();
 }
 
 function loadJson(callback) {
-	d3.json("maps/landkreise_bawue.json", function(error, json) { 
+	d3.json("maps/landkreise_bawue.json", function(error, jsonD) { 
 		if (error!=null) console.log(error);
 
+		json = jsonD;
 		json.features.sort(function (a,b) {
 			if(a.properties.GEN < b.properties.GEN) return -1;
 			if(a.properties.GEN > b.properties.GEN) return 1;
 			return 0;
 		});
 
-		callback(json);
+		callback();
 	});
 }
 
 loadDaten(function () {
-	loadJson(function (json) {
+	loadJson(function () {
 		var str = "";
 		json.features.map(function (d,k) {
 			for (var i = 0; i < csv.length; i++) {
@@ -76,6 +65,81 @@ loadDaten(function () {
 				}
 			}
 		});
-		showMap(json);
+		showMap();
 	});
+});
+
+function toFloat(str) {
+	str = str.replace( /,/,"." );
+	return parseFloat(str);
+}
+
+function update() {
+	var people = "Bevölkerungsstand 2012";
+
+	console.log(mode);
+	var key = "Zuzüge "+age+" Jahr 2012";
+	if (mode == "in")
+		key = "Zuzüge "+age+" Jahr 2012";
+	if (mode == "out")
+		key = "Fortzüge "+age+" Jahr 2012";
+
+	var dataMax = d3.max(json.features, function (d) {
+		var migration = toFloat(d.properties.daten[key]);
+		var bev = toFloat(d.properties.daten[people]);
+		var normalized = migration * 100 / bev;
+		return normalized;
+	});
+	console.log('max/min:', dataMax, 0);
+
+	targetColor = "#000099";
+	if (mode == "in") targetColor = "#009900";
+	if (mode == "out") targetColor = "#990000";
+	var colorMap = d3.scale.linear().domain([0, 5]).range(["#cccccc", targetColor]);
+
+	kreise
+		.attr("fill", function (d, i) {
+			var migration = toFloat(d.properties.daten[key]);
+			var bev = toFloat(d.properties.daten[people]);
+			var normalized = migration * 100 / bev;
+//			console.log(i, d.properties.GEN, bev, colorMap(bev));
+			return colorMap(normalized);
+		})
+		.on('mouseover', function (d) {
+			var migration = toFloat(d.properties.daten[key]);
+			var bev = toFloat(d.properties.daten[people]);
+			var normalized = migration * 100 / bev;
+
+			console.log(d.properties.GEN);
+			var coordinates = [0, 0];
+			coordinates = d3.mouse(this);
+			var x = coordinates[0];
+			var y = coordinates[1];
+			d3.select('#tooltip')
+				.style('top', function () { return (y+100)+"px"})
+				.style('left', function () { return x+"px"})
+				.text(d.properties.GEN+" "+normalized.toFixed(2)+"/100 EW");
+		});
+};
+
+d3.select('#btnIn').on('click', function () {
+	mode = "in";
+	update();
+});
+d3.select('#btnOut').on('click', function () {
+	mode = "out";
+	update();
+});
+d3.select('#btnDiff').on('click', function () {
+	mode = "diff";
+	update();
+});
+
+d3.select('#btn18-25').on('click', function () {
+	age = "18-25";
+	update();
+});
+d3.select('#btn25-30').on('click', function () {
+	age = "25-30";
+	update();
 });
